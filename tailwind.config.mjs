b8d032d92e256cb8fece8cdd815f03a6b3a1e0e4
TJ Draper <tj@buzzingpixel.com>
2023-11-06 16:54:47 -0600

    Add tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'tjd-red': {
+                    100: '#f5c4c0',
+                    200: '#eb8a81',
+                    300: '#e15042',
+                    400: '#bd2b1d',
+                    500: '#7f1d14',
+                    600: '#65170f',
+                    700: '#4c110b',
+                    800: '#320b07',
+                    900: '#190503',
+                },
+            },
+        },
     },
     plugins: [
         require('@tailwindcss/typography'),
