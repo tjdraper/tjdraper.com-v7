8ee4d20cb959bbb8cfbfa934aee6d93bd1be335b
TJ Draper <tj@buzzingpixel.com>
2023-11-06 16:59:26 -0600

    Add index listing of blog posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ import { defineCollection, z } from 'astro:content';
 const blog = defineCollection({
     schema: z.object({
         title: z.string(),
+        preview: z.string(),
     }),
 });
 
