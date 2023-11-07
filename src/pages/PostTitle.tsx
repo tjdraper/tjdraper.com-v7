df2dd6f7437a48372cf6f38d469fa73c93675bd9
TJ Draper <tj@buzzingpixel.com>
2023-11-07 11:20:20 -0600

    Add (Link Post) to blog listing page on link posts

---
+++ src/pages/PostTitle.tsx
@@ -0,0 +1,34 @@
+import { type Post } from './blog/Post';
+
+export default function Component (
+    {
+        post,
+    }: {
+        post: Post;
+    },
+) {
+    if (!post.data.link) {
+        return (
+            <a
+                className="text-tjd-red-500 hover:text-tjd-red-600 inline-block align-middle"
+                href={`/blog/${post.slug}`}
+            >
+                {post.data.title}
+            </a>
+        );
+    }
+
+    return (
+        <>
+            <a
+                className="text-tjd-red-500 hover:text-tjd-red-600 inline-block align-middle"
+                href={`/blog/${post.slug}`}
+            >
+                {post.data.title}
+            </a>
+            <span className="text-gray-500 text-xl inline-block ml-2 align-middle font-normal">
+                (Link Post)
+            </span>
+        </>
+    );
+}
